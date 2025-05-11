2aee7c294bfec8cde7c2267d83771ae6fdab4188
amrasmar27 <s12324208@stu.najah.edu>
2025-05-11 19:16:29 +0300

    settings

---
+++ job-portal/src/app/job-seeker/settings/settings.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-settings',
+  imports: [ReactiveFormsModule],
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.css'
+})
+export class SettingsComponent {
+  profileForm = new FormGroup({
+  fullName: new FormControl(''),
+  phone: new FormControl(''),
+  gender: new FormControl(''),
+  city: new FormControl(''),
+  educationLevel: new FormControl(''),
+  major: new FormControl(''),
+  experienceYears: new FormControl('')
+  });
+
+}
--- job-portal/src/app/job-seeker/side-bar/side-bar.component.ts
+++ job-portal/src/app/job-seeker/side-bar/side-bar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { PeopleYouMayKnowComponent } from '../people-you-may-know/people-you-may-know.component';
+import { SettingsComponent } from '../settings/settings.component';
+import { CommonModule } from '@angular/common'; 
 @Component({
   selector: 'app-side-bar',
-  imports: [PeopleYouMayKnowComponent],
+  imports: [PeopleYouMayKnowComponent,SettingsComponent,CommonModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
 export class SideBarComponent {
+  showSettings = false;
 
+  toggleSettings() {
+    this.showSettings = !this.showSettings;
+  }
 }
