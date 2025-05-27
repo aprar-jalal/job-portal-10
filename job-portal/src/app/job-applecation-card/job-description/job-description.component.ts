13479c93cd615781b091fdce9a2dc45940c42cfd
aprar-jalal <s12324682@stu.najah.edu>
2025-05-27 12:14:38 -0700

    new uapdates

--- job-portal/src/app/job-applecation-card/job-description/job-description.component.ts
+++ job-portal/src/app/job-applecation-card/job-description/job-description.component.ts
@@ -1,16 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component ,OnInit } from '@angular/core';
-import {ActivatedRoute, RouterLink} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { JobDataService } from '../serves/job-data.service';
 import { JobDataLocation } from '../models/job-data-location';
 import Swal from 'sweetalert2';
 
-
 @Component({
   selector: 'app-job-description',
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './job-description.component.html',
-  styleUrl: './job-description.component.css'
+  styleUrl: './job-description.component.css',
 })
 export class JobDescriptionComponent implements OnInit {
   job?: JobDataLocation;
@@ -23,13 +22,12 @@ export class JobDescriptionComponent implements OnInit {
     const jobId = Number(this.route.snapshot.paramMap.get('id'));
     this.job = this.jobService.getJobById(jobId);
   }
- 
-  showApplecationMsg(){
-  Swal.fire({
-  title: "Applection Done!",
-  icon: "success",
-  draggable: true
- });
-}
 
+  showApplecationMsg() {
+    Swal.fire({
+      title: 'Applection Done!',
+      icon: 'success',
+      draggable: true,
+    });
+  }
 }
