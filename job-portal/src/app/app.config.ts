6b6db499214e0ac7dc37ed3d89449460b2b161a9
amrasmar27 <s12324208@stu.najah.edu>
2025-05-31 17:32:54 +0300

    Fix conflicts and update app config, routes and package files

--- job-portal/src/app/app.config.ts
+++ job-portal/src/app/app.config.ts
@@ -3,12 +3,9 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-<<<<<<< Updated upstream
+
   providers: [ provideHttpClient(),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
-=======
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),provideHttpClient()]
->>>>>>> Stashed changes
+
 };
